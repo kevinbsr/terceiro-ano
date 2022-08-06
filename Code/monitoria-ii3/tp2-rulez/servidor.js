eb5f9ebd8cf4b10adf698e129bdd475b3447143b
Kevin Benevides <61016019+kevinbsr@users.noreply.github.com>
2022-08-06 14:41:06 -0300

    Express server with multiple routes

--- Code/monitoria-ii3/tp2-rulez/servidor.js
+++ Code/monitoria-ii3/tp2-rulez/servidor.js
@@ -1,35 +1,45 @@
-/* Servidor JS */
-
 const express = require('express')
 const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
 app.get('/', (requisicao, resposta) => {
-  resposta.render('conta', { soma: 0 })
+  resposta.render('conta', { result: '' })
 })
 
 app.get('/operacoes', (req, res) => {
-  //result = parseFloat(n1.value) + parseFloat(n2.value);
-  //document.getElementById("resultado").innerHTML = result;
+  console.log(req.query.operacao)
 
   if (req.query.operacao == 'soma') {
-    let soma = parseFloat(req.query.N1) + parseFloat(req.query.N2);
-    console.log('Resultado: ' + soma);
-    res.render('conta', {result: soma});
-  } else if (req.query.operacao = 'subtracao') {
-    let subtracao = parseFloat(req.query.N1) - parseFloat(req.query.N2);
-    console.log('Resultado: ' + subtracao);
-    res.render('conta', {result: subtracao})
-  } else if (req.query.opercao = 'multiplicacao') {
+    let soma = parseFloat(req.query.N1) + parseFloat(req.query.N2)
+    console.log('Resultado: ' + soma)
+    res.render('conta', { result: soma })
+  } else if (req.query.operacao == 'subtracao') {
+    let subtracao = parseFloat(req.query.N1) - parseFloat(req.query.N2)
+    console.log('Resultado: ' + subtracao)
+    res.render('conta', { result: subtracao })
+  } else if (req.query.operacao == 'multiplicacao') {
     let multiplicacao = parseFloat(req.query.N1) * parseFloat(req.query.N2)
-    console.log('Resultado: ' + multiplicacao);
-    req.render('conta', {result: multiplicacao})
-  } else if (req.query.operacao = 'divisao') {
+    console.log('Resultado: ' + multiplicacao)
+    res.render('conta', { result: multiplicacao })
+  } else if (req.query.operacao == 'divisao') {
     let divisao = parseFloat(req.query.N1) / parseFloat(req.query.N2)
     console.log('Resultado: ' + divisao)
-    req.render('conta', {result: divisao})
+    res.render('conta', { result: divisao })
+  } else if (req.query.operacao == 'fatorial') {
+    if (req.query.N1 == 0) {
+      result = 1
+    }
+    var resp = req.query.N1
+    while (req.query.N1 > 2) {
+      resp *= --req.query.N1
+      result = resp
+    }
+    res.render('conta', { result: result })
+  } else {
+    console.log('Operação inválida')
+    res.render('conta', { result: 'Operação inválida' })
   }
 })
 
 console.log('Servidor funcionando...')
-app.listen(8888)
+app.listen(8080)
